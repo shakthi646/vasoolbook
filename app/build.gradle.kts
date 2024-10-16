d8a8f596bdae4a895d60a2e2817f014fb2fa7102
Sathyaprakash <sathyaprakash.k@zohocorp.com>
2024-10-16 18:06:38 +0530

    version name code updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ksp.vasool"
         minSdk = 25
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
